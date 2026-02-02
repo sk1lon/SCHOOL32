f92cf55cd4800228a0c44527080b629e3ed726b3
sk1lon <two.trans@mail.ru>
2026-02-02 12:34:50 -0800

    Update Flask to use Render PORT and 0.0.0.0

--- app.py
+++ app.py
@@ -85,6 +85,5 @@ def get_events():
 
     return jsonify(events)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
