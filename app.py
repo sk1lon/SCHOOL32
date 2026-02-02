29bf2d410ecbb8c6668f805435e6066d37200e76
sk1lon <two.trans@mail.ru>
2026-02-02 12:42:54 -0800

    Update Flask to use Render PORT and 0.0.0.0

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify, make_response, redirect
 import sqlite3
 from models import DB
@@ -84,6 +85,8 @@ def get_events():
     ]
 
     return jsonify(events)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
